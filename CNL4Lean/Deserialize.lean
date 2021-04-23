733f4247d2aacc3d05b4354db62883c2277db7ff
Adrian Marti <adrianmarti06@gmail.com>
2021-04-23 14:43:25 +0200

    Speed up compilation
    It's **much, much** faster now, but still not fast. Oh well.

--- CNL4Lean/Deserialize.lean
+++ CNL4Lean/Deserialize.lean
@@ -19,6 +19,8 @@ inductive DeserializationError where
   | unsupportedFeature : String -> DeserializationError
   | parsingError : String -> DeserializationError
 
+abbrev DeserializeM := ExceptT DeserializationError IO
+
 open DeserializationError
 
 instance : Inhabited DeserializationError where
@@ -33,29 +35,30 @@ instance : ToString DeserializationError where
 def throwUnexpected {α: Type u} {m: Type u -> Type v} [MonadExcept DeserializationError m] (s: String) (json: Json) : m α :=
   throw (unexpectedValue s json)
 
-class Deserializable (α: Type u) (m: Type u -> Type v) [MonadExcept DeserializationError m] where
-  deserialize : Json -> m α
+-- I originally used this typeclass for this module:
+-- variable [Monad m] [MonadExcept DeserializationError m]
+-- But then this module needs like 10 minutes to compile.
+-- This is so odd.
+
+class Deserializable (α: Type) where
+  deserialize : Json -> DeserializeM α
 
 open Deserializable
-
-section Deserializable
-
-variable [Monad m] [MonadExcept DeserializationError m]
 
 -- In order to implement `partial`, lean needs to prove that the partial
 -- functions we are constructing actually have a total implementation.
 -- We provide this implementation implicitly by providing `Inhabited`.
-instance : Inhabited (m α) where
+instance : Inhabited (DeserializeM α) where
   default := throw arbitrary
 
 
-instance [Deserializable α m] : Deserializable (Array α) m where
+instance [Deserializable α] : Deserializable (Array α) where
   deserialize
     | Json.arr a => a.mapM deserialize
     | json => throwUnexpected "array" json
 
 
-instance [Deserializable α m] : Deserializable (Option α) m where
+instance [Deserializable α] : Deserializable (Option α) where
   deserialize
     | Json.null => none
     | json => some <$> deserialize json
@@ -68,7 +71,7 @@ private instance : Monad Option where
     | none => none
     | some t => f t
 
-/- Gets an inductive type constructor name and its arguments in Json format.
+/-- Gets an inductive type constructor name and its arguments in Json format.
 For zero arguments, we return `Json.null`
 For one argument we return the json value of the argument,
 and for multiple arguments, we return a json array of json values -/
@@ -83,7 +86,7 @@ where
     (tag, contents)
 
 
-instance : Deserializable Delim m where
+instance : Deserializable Delim where
   deserialize
     | "Invis" => Delim.invis
     | "Paren" => Delim.invis
@@ -92,7 +95,7 @@ instance : Deserializable Delim m where
     | json => throwUnexpected "delimiter" json
 
 
-instance : Deserializable Tok m where
+instance : Deserializable Tok where
   deserialize json := match getInductive? json with
     | some ("Word", Json.str t) => Tok.word t
     | some ("Variable", Json.str t) => Tok.variable' t
@@ -104,14 +107,14 @@ instance : Deserializable Tok m where
     | _ => throwUnexpected "token" json
 
 
-instance : Deserializable VarSymbol m where
+instance : Deserializable VarSymbol where
   deserialize json := match getInductive? json with
     | some ("NamedVar", Json.str t) => VarSymbol.namedVar t
     | some ("FreshVar", (n: Int)) => VarSymbol.freshVar n
     | _ => throwUnexpected "variable symbol" json
 
 
-private partial def deserializeExpr (json: Json) : m Expr' := match getInductive? json with
+private partial def deserializeExpr (json: Json) : DeserializeM Expr' := match getInductive? json with
     | some ("ExprVar", varSymb) => Expr'.var <$> deserialize varSymb
     | some ("ExprInteger", (n : Int)) => Expr'.int n
     | some ("ExprConst", t) => Expr'.const <$> deserialize t
@@ -121,17 +124,17 @@ private partial def deserializeExpr (json: Json) : m Expr' := match getInductive
       Expr'.mixfix pattern args
     | _ => throwUnexpected "expression" json
 
-instance : Deserializable Expr' m where
+instance : Deserializable Expr' where
   deserialize := deserializeExpr
 
-instance : Deserializable Sign m where
+instance : Deserializable Sign where
   deserialize
     | "Positive" => Sign.positive
     | "Negative" => Sign.negative
     | json => throwUnexpected "sign" json
 
 
-private partial def deserializeChain (json: Json) : m Chain := match getInductive? json with
+private partial def deserializeChain (json: Json) : DeserializeM Chain := match getInductive? json with
   | some ("ChainBase", Json.arr #[Json.arr leftExprs, sgn, rel, Json.arr rightExprs]) => do
     let leftExprs <- leftExprs.mapM deserialize
     let sgn <- deserialize sgn
@@ -147,24 +150,24 @@ private partial def deserializeChain (json: Json) : m Chain := match getInductiv
   | _ => throwUnexpected "chain" json
 
 
-instance : Deserializable Chain m where
+instance : Deserializable Chain where
   deserialize := deserializeChain
 
-instance : Deserializable SymbolicPredicate m where
+instance : Deserializable SymbolicPredicate where
   deserialize
     | Json.arr #[Json.str pred, (arity : Int)] => SymbolicPredicate.mk pred arity
     | json => throwUnexpected "symbolic predicate" json
 
 
-instance : Deserializable Formula m where
-  deserialize (json: Json) : m Formula := match getInductive? json with
+instance : Deserializable Formula where
+  deserialize (json: Json) : DeserializeM Formula := match getInductive? json with
   | some ("FormulaChain", t) => Formula.chain <$> deserialize t
   | some ("FormulaPredicate", Json.arr #[pred, Json.arr args]) =>       
     Formula.predicate <$> deserialize pred <*> args.mapM deserialize
   | _ => throwUnexpected "formula" json
 
-instance : Deserializable SgPl m where
-  deserialize (json: Json) : m SgPl := do
+instance : Deserializable SgPl where
+  deserialize (json: Json) : DeserializeM SgPl := do
     let sg <- match json.getObjVal? "sg" with
       | some sg => deserialize sg
       | none => throwUnexpected "SgPl" json
@@ -175,75 +178,67 @@ instance : Deserializable SgPl m where
     
     pure { sg := sg, pl := pl }
 
-abbrev DeserializeM := ExceptT DeserializationError IO
+mutual
+  partial def deserializeTerm (json: Json): DeserializeM Term := match getInductive? json with
+    | some ("TermExpr", t) => Term.expr <$> deserialize t
+    | some ("TermFun", t) => Term.function <$> deserializeFun t
+    | _ => throwUnexpected "term" json
 
-set_option profiler true
+  partial def deserializeFun : Json -> DeserializeM Fun
+    | Json.arr #[sgPl, Json.arr args] => Fun.mk <$> deserialize sgPl <*> args.mapM deserializeTerm'
+    | json => throwUnexpected "functional noun" json
+  where
+    -- This is a workaround for a bug, where the lean kernel doesn't find 
+    -- the `deserializedTerm` namespace when passed to `mapM`
+    deserializeTerm' := deserializeTerm
+end
 
-def f : Json -> DeserializeM SgPl := deserialize
 
-set_option profiler false
-
--- mutual
---   partial def deserializeTerm (json: Json): m Term := match getInductive? json with
---     | some ("TermExpr", t) => Term.expr <$> deserialize t
---     | some ("TermFun", t) => Term.function <$> deserializeFun t
---     | _ => throwUnexpected "term" json
-
---   partial def deserializeFun : Json -> m Fun
---     | Json.arr #[sgPl, Json.arr args] => Fun.mk <$> deserialize sgPl <*> args.mapM deserializeTerm'
---     | json => throwUnexpected "functional noun" json
---   where
---     -- This is a workaround for a bug, where the lean kernel doesn't find 
---     -- the `deserializedTerm` namespace when passed to `mapM`
---     deserializeTerm' := deserializeTerm
--- end
-
-/-
-instance : Deserializable Fun m where
+instance : Deserializable Fun where
   deserialize := deserializeFun
 
-instance : Deserializable Term m where
+instance : Deserializable Term where
   deserialize := deserializeTerm
 
-instance [Deserializable α m]: Deserializable (Noun α) m where
+instance [Deserializable α]: Deserializable (Noun α) where
   deserialize
     | Json.arr #[sgPl, Json.arr args] =>
       Noun.mk <$> deserialize sgPl <*> args.mapM deserialize
     | json => throwUnexpected "noun" json
 
-instance [Deserializable α m]: Deserializable (Adj α) m where
+instance [Deserializable α]: Deserializable (Adj α) where
   deserialize
     | Json.arr #[sgPl, Json.arr args] =>
       Adj.mk <$> deserialize sgPl <*> args.mapM deserialize
     | json => throwUnexpected "adjective" json
 
-instance [Deserializable α m]: Deserializable (Verb α) m where
+instance [Deserializable α]: Deserializable (Verb α) where
   deserialize
     | Json.arr #[sgPl, Json.arr args] =>
       Verb.mk <$> deserialize sgPl <*> args.mapM deserialize
     | json => throwUnexpected "verb" json
 
-instance : Deserializable VerbPhrase m where
-  deserialize (json: Json) : m VerbPhrase := match getInductive? json with
+instance : Deserializable VerbPhrase where
+  deserialize (json: Json) : DeserializeM VerbPhrase := match getInductive? json with
     | some ("VPVerb", t) => VerbPhrase.verb <$> deserialize t
     | some ("VPAdj", t) => VerbPhrase.adj <$> deserialize t
     | some ("VPVerbNot", t) => VerbPhrase.verbNot <$> deserialize t
     | some ("VPAdjNot", t) => VerbPhrase.adjNot <$> deserialize t
     | _ => throwUnexpected "verb phrase" json
 
-instance : Deserializable AdjL m where
+instance : Deserializable AdjL where
   deserialize
     | Json.arr #[sgPl, Json.arr args] =>
       AdjL.mk <$> deserialize sgPl <*> args.mapM deserialize
     | json => throwUnexpected "left adjective" json
 
-instance : Deserializable AdjR m where
-  deserialize (json: Json) : m AdjR := match getInductive? json with
+instance : Deserializable AdjR where
+  deserialize (json: Json) : DeserializeM AdjR := match getInductive? json with
     | some ("AdjR", Json.arr #[sgPl, Json.arr args]) =>
       AdjR.adjR <$> deserialize sgPl <*> args.mapM deserialize
     | _ => throwUnexpected "right adjective" json
 
-instance : Deserializable Connective m where
+instance : Deserializable Connective where
   deserialize
     | "Conjunction" => Connective.conjunction
     | "Disjunction" => Connective.disjunction
@@ -253,15 +248,15 @@ instance : Deserializable Connective m where
     | "NegatedDisjunction" => Connective.negatedDisjunction
     | json => throwUnexpected "connective" json
 
-instance : Deserializable Quantifier m where
+instance : Deserializable Quantifier where
   deserialize
     | "Universally" => Quantifier.universally
     | "Existentially" => Quantifier.existentially
     | "Nonexistentially" => Quantifier.nonexistentially
     | json => throwUnexpected "quantifier" json
 
-instance : Deserializable Bound m where
-  deserialize (json: Json) : m Bound := match getInductive? json with 
+instance : Deserializable Bound where
+  deserialize (json: Json) : DeserializeM Bound := match getInductive? json with 
     | some ("Unbounded", Json.null) => Bound.unbounded
     | some ("Bounded", Json.arr #[sgn, rel, expr]) => Bound.bounded
         <$> deserialize sgn
@@ -270,7 +265,7 @@ instance : Deserializable Bound m where
     | _ => throwUnexpected "bound" json
 
 mutual
-  private partial def deserializeNP : Json -> m NounPhrase
+  private partial def deserializeNP : Json -> DeserializeM NounPhrase
     | Json.arr #[adjL, noun, var?, adjR, stmt?] => NounPhrase.mk
         <$> deserialize adjL
         <*> deserialize noun
@@ -279,7 +274,7 @@ mutual
         <*> deserializeStmt stmt?
     | json => throwUnexpected "noun phrase" json
   
-    private partial def deserializeNPVars : Json -> m NounPhraseVars
+    private partial def deserializeNPVars : Json -> DeserializeM NounPhraseVars
     | Json.arr #[adjL, noun, vars, adjR, stmt?] => NounPhraseVars.mk
         <$> deserialize adjL
         <*> deserialize noun
@@ -288,11 +283,11 @@ mutual
         <*> deserializeStmt stmt?
     | json => throwUnexpected "noun phrase" json
 
-  private partial def deserializeQPhrase : Json -> m QuantPhrase
+  private partial def deserializeQPhrase : Json -> DeserializeM QuantPhrase
     | Json.arr #[quant, np] => QuantPhrase.mk <$> deserialize quant <*> deserializeNPVars np
     | json => throwUnexpected "quantified phrase" json
 
-  private partial def deserializeStmt (json: Json) : m Stmt := match getInductive? json with
+  private partial def deserializeStmt (json: Json) : DeserializeM Stmt := match getInductive? json with
     | some ("StmtFormula", t) => Stmt.formula <$> deserialize t
     | some ("StmtVerbPhrase", Json.arr #[term, verbPhrase]) => Stmt.verbPhrase
         <$> deserialize term
@@ -318,19 +313,19 @@ mutual
     | _ => throwUnexpected "statement" json
 end
 
-instance : Deserializable NounPhrase m where
+instance : Deserializable NounPhrase where
   deserialize := deserializeNP
 
-instance : Deserializable NounPhraseVars m where
+instance : Deserializable NounPhraseVars where
   deserialize := deserializeNPVars
 
-instance : Deserializable QuantPhrase m where
+instance : Deserializable QuantPhrase where
   deserialize := deserializeQPhrase
 
-instance : Deserializable Stmt m where
+instance : Deserializable Stmt where
   deserialize := deserializeStmt
 
-instance : Deserializable DefnHead m where
+instance : Deserializable DefnHead where
   deserialize json := match getInductive? json with
     | some ("DefnAdj", Json.arr #[np, varSymb, adj]) => DefnHead.adj
         <$> deserialize np
@@ -352,7 +347,7 @@ instance : Deserializable DefnHead m where
         <*> deserialize varSymbs
     | _ => throwUnexpected "definition head" json
 
-instance : Deserializable Asm m where
+instance : Deserializable Asm where
   deserialize json := match getInductive? json with
     | some ("AsmSuppose", stmt) => Asm.suppose <$> deserialize stmt
     | some ("AsmLetNoun", Json.arr #[vs, np]) => Asm.letNoun
@@ -369,7 +364,7 @@ instance : Deserializable Asm m where
         <*> deserialize expr
     | _ => throwUnexpected "assumption" json
 
-instance : Deserializable Defn m where
+instance : Deserializable Defn where
   deserialize json := match getInductive? json with
     | some ("Defn", Json.arr #[asms, defnHead, stmt]) => Defn.defn
         <$> deserialize asms
@@ -382,21 +377,21 @@ instance : Deserializable Defn m where
         <*> deserialize term
     | _ => throwUnexpected "definition" json
 
-instance : Deserializable Axiom m where
+instance : Deserializable Axiom where
   deserialize json := match getInductive? json with
     | some ("Axiom", Json.arr #[asms, stmt]) => Axiom.mk
         <$> deserialize asms
         <*> deserialize stmt
     | _ => throwUnexpected "axiom" json
 
-instance : Deserializable Lemma m where
+instance : Deserializable Lemma where
   deserialize json := match getInductive? json with
     | some ("Lemma", Json.arr #[asms, stmt]) => Lemma.mk
         <$> deserialize asms
         <*> deserialize stmt
     | _ => throwUnexpected "lemma" json
 
-instance : Deserializable Para m where
+instance : Deserializable Para where
   deserialize json := match getInductive? json with
     | some ("ParaDefn", defn) => Para.defn' <$> deserialize defn
     | some ("ParaLemma", Json.arr #[tag, lem]) => Para.lemma'
@@ -404,9 +399,18 @@ instance : Deserializable Para m where
         <*> deserialize lem
     | _ => throwUnexpected "paragraph" json
 
-end Deserializable
+-- Wow, removing the monad transformer typeclass from Deserialize 
+-- improves the compilation time of this from 3:25min to 1.5 sec.
+-- It's still a lot, but I can work with it.
 
-abbrev DeserializeM := ExceptT DeserializationError IO
+-- I guess the problem was that every usage of `deserialize` triggered
+-- the typeclass instance problem for the monad transformer, since
+-- the monad transformer was a typeclass parameter. But the profiler
+-- says that the complexity lied in `compilation` not in
+-- `typeclass inference`. I am still not sure what the issue was,
+-- but maybe moving the typeclass parameter to an explicit param
+-- of `deserialize` would also have solved the issue.
 
-def deserialize' : Json -> DeserializeM Para := deserialize -/
--- This takes 3:25 min for getting processed. This is not normal
+-- set_option profiler true
+-- def deserialize' : DeserializeM Para := Deserializable.deserialize Json.null
+
